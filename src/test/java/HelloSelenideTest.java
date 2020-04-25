3105d87bd17354002b3ab2eb1726bfc129b3fbcc
Cristian Luis Pereira <cl.pereira08@gmail.com>
2020-04-25 15:53:21 -0300

    Commit inicial verificando se o Ninja Plus esta rodando

---
+++ src/test/java/HelloSelenideTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.title;
+import static com.codeborne.selenide.WebDriverRunner.isFirefox;
+import static org.testng.Assert.assertEquals;
+
+public class HelloSelenideTest {
+    @Test
+    public void OnAir() {
+        isFirefox();
+        open("http://ninjaplus-web:5000/login");
+        assertEquals(title(), "Ninja+");
+    }
+}
