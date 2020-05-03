3d3c0b73a38500b8206871af06bf9034fb820d4f
Cristian Luis Pereira <cl.pereira08@gmail.com>
2020-05-02 23:04:30 -0300

    Adicao de imagens de evidencia aos relatorios do Allure

--- src/test/java/common/BaseTest.java
+++ src/test/java/common/BaseTest.java
@@ -1,10 +1,21 @@
 package common;
 
 import com.codeborne.selenide.Configuration;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.LoginPage;
 import pages.MoviePage;
 import pages.Sidebar;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.screenshot;
+import static io.qameta.allure.Allure.addAttachment;
 
 public class BaseTest {
     protected static LoginPage loginPage;
@@ -20,5 +31,20 @@ public class BaseTest {
         loginPage = new LoginPage();
         sidebar = new Sidebar();
         moviePage = new MoviePage();
+    }
+
+    @AfterMethod
+    public void screenshotEvidence() {
+        String tempShot = screenshot("temp_shot");
+
+        try {
+            BufferedImage bufferedImage = ImageIO.read(new File(tempShot));
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImage, "png", byteArrayOutputStream);
+            byte[] finalShot = byteArrayOutputStream.toByteArray();
+            addAttachment("evidencia", new ByteArrayInputStream(finalShot));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
